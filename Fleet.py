54bbe0406ee85eec247b58bb65879297909ccf79
unknown <pedelgadillo@gmail.com>
2021-10-07 14:49:38 -0500

    Added names for the weapons and the robots

--- Fleet.py
+++ Fleet.py
@@ -9,18 +9,18 @@ class Fleet:
         self.make_fleet()
 
     def weapon_create(self):
-        weapon0 = Weapon("",50)
-        weapon1 = Weapon("",50)
-        weapon2 = Weapon("",50)
+        weapon0 = Weapon("The Laser Sword",50)
+        weapon1 = Weapon("The Sword That Shoots Lasers",50)
+        weapon2 = Weapon("The Sword Laser",50)
         self.armory.append(weapon0)
         self.armory.append(weapon1)
         self.armory.append(weapon2)
     
     
     def make_fleet(self):
-        robot0 = Robot('')
-        robot1 = Robot('')
-        robot2 = Robot('')
+        robot0 = Robot('Killer Term', "The Laser Sword")
+        robot1 = Robot('Murderest Kim', "The Sword That Shoots Lasers")
+        robot2 = Robot('Pyscho Bob', "The Sword Laser")
         self.robots.append(robot0)
         self.robots.append(robot1)
         self.robots.append(robot2)     
--- Herd.py
+++ Herd.py
@@ -6,9 +6,9 @@ class Herd:
         self.herd_up()
 
     def herd_up(self):
-        dino0 = Dinosaur("Timmy Two Rex", 35)
-        dino1 = Dinosaur("One Eyed Bill", 35)
-        dino2 = Dinosaur("Veli The Raptor", 35)
+        dino0 = Dinosaur("Timmy Two Rex", 200)
+        dino1 = Dinosaur("One Eyed Bill", 200)
+        dino2 = Dinosaur("Veli The Raptor", 200)
         self.dinosaurs.append(dino0)
         self.dinosaurs.append(dino1)
         self.dinosaurs.append(dino2)
